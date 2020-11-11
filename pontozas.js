56bff6047810040de130f24e88497b90b427fd6d
szgery <szabogergely805@gmail.com>
2020-11-11 19:04:56 +0000

    Fixing the worst js i have ever seen

---
+++ pontozas.js
@@ -0,0 +1,14 @@
+function Szamol(){
+    var matekIrasbeli = document.getElementById("irasbeliMat").value;
+    var matekSzobeli = document.getElementById("irasbeliMat").value;
+    var toriIrasbeli = document.getElementById("irasbeliTor").value;
+    var toriSzobeli = document.getElementById("szobeliTor").value;
+    var magyarIrasbeli = document.getElementById("magyar").value;
+    var magyarSzobeli = document.getElementById("magyar").value;
+    var nyelvIrasbeli = document.getElementById("nyelv").value;
+    var nyelvSzobeli = document.getElementById("nyelv").value;
+
+    if(matekIrasbeli = 100){
+        document.getElementById("eredmenyasd").innerHTML = "Matek 5"
+    }
+}
