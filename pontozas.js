d2e413f5d36d3fc875025eb554932193c3a07b7d
szgery <szabogergely805@gmail.com>
2020-11-11 21:24:23 +0000

    Finally, finished, and working!!!

--- pontozas.js
+++ pontozas.js
@@ -1,64 +1,64 @@
 function Szamol(){
     var matekIrasbeli = parseInt(document.getElementById("irasbeliMat").value);
-    var matekSzobeli = document.getElementById("irasbeliMat").value;
-    var toriIrasbeli = document.getElementById("irasbeliTor").value;
-    var toriSzobeli = document.getElementById("szobeliTor").value;
-    var magyarIrasbeli = document.getElementById("irasbeliMagy").value;
-    var magyarSzobeli = document.getElementById("szobeliMagy").value;
-    var nyelvIrasbeli = document.getElementById("irasbeliNyelv").value;
-    var nyelvSzobeli = document.getElementById("szobeliNyelv").value;
+    var matekSzobeli = parseInt(document.getElementById("szobeliMat").value);
+    var toriIrasbeli = parseInt(document.getElementById("irasbeliTor").value);
+    var toriSzobeli = parseInt(document.getElementById("szobeliTor").value);
+    var magyarIrasbeli = parseInt(document.getElementById("irasbeliMagy").value);
+    var magyarSzobeli = parseInt(document.getElementById("szobeliMagy").value);
+    var nyelvIrasbeli = parseInt(document.getElementById("irasbeliNyelv").value);
+    var nyelvSzobeli = parseInt(document.getElementById("szobeliNyelv").value);
 
     var stuff = [];
 
     if((matekIrasbeli + matekSzobeli*100/50)/2 <= 100 && (matekIrasbeli + matekSzobeli*100/50)/2 >= 80){
-       stuff[0] = "Matek 5";
-    }
-    else if((matekIrasbeli + matekSzobeli*100/50)/2 <= 79 && (matekIrasbeli + matekSzobeli*100/50)/2 >= 60){
-        stuff[0] = "Matek 4";
-    }else if((matekIrasbeli + matekSzobeli*100/50)/2 <= 59 && (matekIrasbeli + matekSzobeli*100/50)/2 >= 40){
-        stuff[0] = "Matek 3";
-    }else if((matekIrasbeli + matekSzobeli*100/50)/2 <= 39 && (matekIrasbeli + matekSzobeli*100/50)/2 >= 25){
-        stuff[0] = "Matek 2";
-    }else if((matekIrasbeli + matekSzobeli*100/50)/2 <= 24 && (matekIrasbeli + matekSzobeli*100/50)/2 >= 0){
-        stuff[0] = "Matek 1";
-    }
+        stuff[0] = "Matek 5";
+     }
+     else if((matekIrasbeli + matekSzobeli*100/50)/2 <= 79 && (matekIrasbeli + matekSzobeli*100/50)/2 >= 60){
+         stuff[0] = "Matek 4";
+     }else if((matekIrasbeli + matekSzobeli*100/50)/2 <= 59 && (matekIrasbeli + matekSzobeli*100/50)/2 >= 40){
+         stuff[0] = "Matek 3";
+     }else if((matekIrasbeli + matekSzobeli*100/50)/2 <= 39 && (matekIrasbeli + matekSzobeli*100/50)/2 >= 25){
+         stuff[0] = "Matek 2";
+     }else if((matekIrasbeli + matekSzobeli*100/50)/2 <= 24 && (matekIrasbeli + matekSzobeli*100/50)/2 >= 0){
+         stuff[0] = "Matek 1";
+     }
+ 
+     if((toriIrasbeli + toriSzobeli*100/60)/2 <= 100 && (toriIrasbeli + toriSzobeli*100/60)/2 >= 80){
+         stuff[1] = "Töri 5";
+     }
+     else if((toriIrasbeli + toriSzobeli*100/60)/2 <= 79 && (toriIrasbeli + toriSzobeli*100/60)/2 >= 60){
+         stuff[1] = "Töri 4";
+     }else if((toriIrasbeli + toriSzobeli*100/60)/2 <= 59 && (toriIrasbeli + toriSzobeli*100/60)/2 >= 40){
+         stuff[1] = "Töri 3";
+     }else if((toriIrasbeli + toriSzobeli*100/60)/2 <= 39 && (toriIrasbeli + toriSzobeli*100/60)/2 >= 25){
+         stuff[1] = "Töri 2";
+     }else if((toriIrasbeli + toriSzobeli*100/60)/2 <= 24 && (toriIrasbeli + toriSzobeli*100/60)/2 >= 0){
+         stuff[1] = "Töri 1";
+     }
+ 
+     if((magyarIrasbeli + magyarSzobeli*100/50)/2 <=100 && (magyarIrasbeli + magyarSzobeli*100/50)/2 >=80){
+         stuff[2] = "Magyar 5";
+     }
+     else if((magyarIrasbeli + magyarSzobeli*100/50)/2 <=79 && (magyarIrasbeli + magyarSzobeli*100/50)/2 >=60){
+         stuff[2] = "Magyar 4";
+     }else if((magyarIrasbeli + magyarSzobeli*100/50)/2 <=59 && (magyarIrasbeli + magyarSzobeli*100/50)/2 >=40){
+         stuff[2] = "Magyar 3";
+     }else if((magyarIrasbeli + magyarSzobeli*100/50)/2 <=39 && (magyarIrasbeli + magyarSzobeli*100/50)/2 >=25){
+         stuff[2] = "Magyar 2";
+     }else if((magyarIrasbeli + magyarSzobeli*100/50)/2 <=24 && (magyarIrasbeli + magyarSzobeli*100/50)/2 >=0){
+         stuff[2] = "Magyar 1";
+     }
 
-    if(toriIrasbeli < 101 && toriIrasbeli >= 80){
-        stuff[1] = "Töri 5";
-    }
-    else if(toriIrasbeli < 80 && toriIrasbeli >= 60){
-        stuff[1] = "Töri 4";
-    }else if(toriIrasbeli < 60 && toriIrasbeli >= 40){
-        stuff[1] = "Töri 3";
-    }else if(toriIrasbeli< 40 && toriIrasbeli >= 25){
-        stuff[1] = "Töri 2";
-    }else if(toriIrasbeli< 25 && toriIrasbeli >= 0){
-        stuff[1] = "Töri 1";
-    }
-
-    if(magyarIrasbeli < 101 && magyarIrasbeli >= 80){
-        stuff[2] = "Magyar 5";
-    }
-    else if(magyarIrasbeli < 80 && magyarIrasbeli >= 60){
-        stuff[2] = "Magyar 4";
-    }else if(magyarIrasbeli < 60 && magyarIrasbeli >= 40){
-        stuff[2] = "Magyar 3";
-    }else if(magyarIrasbeli< 40 && magyarIrasbeli >= 25){
-        stuff[2] = "Magyar 2";
-    }else if(magyarIrasbeli< 25 && magyarIrasbeli >= 0){
-        stuff[2] = "Magyar 1";
-    }
-
-    if(nyelvIrasbeli < 101 && nyelvIrasbeli >= 80){
+    if((nyelvIrasbeli + nyelvSzobeli*100/33)/2 <= 100 && (nyelvIrasbeli + nyelvSzobeli*100/33)/2 >= 80){
         stuff[3] = "Nyelv 5";
     }
-    else if(nyelvIrasbeli < 80 && nyelvIrasbeli >= 60){
+    else if((nyelvIrasbeli + nyelvSzobeli*100/33)/2 <= 79 && (nyelvIrasbeli + nyelvSzobeli*100/33)/2 >= 60){
         stuff[3] = "Nyelv 4";
-    }else if(nyelvIrasbeli < 60 &&nyelvIrasbeli >= 40){
+    }else if((nyelvIrasbeli + nyelvSzobeli*100/33)/2 <= 59 && (nyelvIrasbeli + nyelvSzobeli*100/33)/2 >= 40){
         stuff[3] = "Nyelv 3";
-    }else if(nyelvIrasbeli< 40 && nyelvIrasbeli >= 25){
+    }else if((nyelvIrasbeli + nyelvSzobeli*100/33)/2 <= 39 && (nyelvIrasbeli + nyelvSzobeli*100/33)/2 >= 25){
         stuff[3] = "Nyelv 2";
-    }else if(nyelvIrasbeli< 25 && nyelvIrasbeli >= 0){
+    }else if((nyelvIrasbeli + nyelvSzobeli*100/33)/2 <= 24 && (nyelvIrasbeli + nyelvSzobeli*100/33)/2 >= 0){
         stuff[3] = "Nyelv 1";
     }
 
